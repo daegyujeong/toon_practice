c15af5fdbfbb0166746e0e574a4f8c0f2d4dcf4b
DaegyuJeong <k5946k@gmail.com>
2024-06-02 15:23:49 +0900

    use stateful widget to update the UI when the data is loaded from the API

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toon_practice/screens/home_screen.dart';
-import 'package:toon_practice/services/api_service.dart';
 
 void main() {
-  ApiService().getTodaysToons();
   runApp(const App());
 }
 
